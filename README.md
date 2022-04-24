297faea5de4e34d5b763377475343c038558f27e
Burak Sakalli <buraksakalli06@gmail.com>
2022-04-24 14:48:57 +0400

    docs(readme): Readme added

---
+++ README.md
@@ -0,0 +1,26 @@
+# React Iskelet
+
+React Iskelet(iskelet means skeleton in Turkish), provides a tiny skeleton loaders.
+
+## Usage
+
+```tsx
+import Skeleton from "react-iskelet";
+
+<Skeleton type={option} />;
+```
+
+Skeleton component extended with **div** element, allows manipulating **div** attributes like `className.`
+
+## Available Types
+
+| Type      |
+| --------- |
+| text      |
+| title     |
+| thumbnail |
+| image     |
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ function cn(...classes: string[]) {
 }
 
 export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
-  type?: "text" | "title" | "thumbnail" | "image" | "select";
+  type?: "text" | "title" | "thumbnail" | "avatar";
 }
 
 const SkeletonChildren = ({ type, ...props }: SkeletonProps) => {
@@ -17,8 +17,8 @@ const SkeletonChildren = ({ type, ...props }: SkeletonProps) => {
       return <Title {...props} />;
     case "thumbnail":
       return <Thumbnail {...props} />;
-    case "image":
-      return <Image {...props} />;
+    case "avatar":
+      return <Avatar {...props} />;
     default:
       return <Text {...props} />;
   }
@@ -37,11 +37,11 @@ const Title = ({ ...props }: React.HTMLAttributes<HTMLDivElement>) => (
 const Thumbnail = ({ ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div className={cn("skeleton__thumbnail", props?.className!)} />
 );
-const Image = ({ ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("skeleton__image", props?.className!)} />
+const Avatar = ({ ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("skeleton__avatar", props?.className!)} />
 );
 
 Skeleton.Text = Text;
 Skeleton.Title = Title;
-Skeleton.Image = Image;
+Skeleton.Avatar = Avatar;
 Skeleton.Thumbnail = Thumbnail;
